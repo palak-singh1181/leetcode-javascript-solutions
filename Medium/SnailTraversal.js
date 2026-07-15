199273fa1045bb771b9f11c909c9d7113d8e8978
Palak Singh <palaksingh9503@gmail.com>
2026-07-14 21:51:31 -0700

    Added to leetcode of this questions

---
+++ Medium/SnailTraversal.js
@@ -0,0 +1,36 @@
+Array.prototype.snail = function(rowsCount, colsCount) {
+
+    if (rowsCount * colsCount !== this.length)
+        return [];
+
+    const result = Array.from(
+        { length: rowsCount },
+        () => Array(colsCount)
+    );
+
+    let index = 0;
+
+    for (let col = 0; col < colsCount; col++) {
+
+        if (col % 2 === 0) {
+
+            for (let row = 0; row < rowsCount; row++) {
+                result[row][col] = this[index++];
+            }
+
+        } else {
+
+            for (let row = rowsCount - 1; row >= 0; row--) {
+                result[row][col] = this[index++];
+            }
+        }
+    }
+
+    return result;
+};
+
+// Example
+
+const nums = [1,2,3,4,5,6];
+
+console.log(nums.snail(3,2));
