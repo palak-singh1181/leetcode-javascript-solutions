cb8cb9bbccf9f9de1be0762a8030e355eaf5f6f2
Palak Singh <palaksingh9503@gmail.com>
2026-07-26 03:34:49 -0700

    Added this solution of leetcode

---
+++ Easy/ArgumentsLength.js
@@ -0,0 +1,8 @@
+function argumentsLength(...args) {
+    return args.length;
+}
+
+// Test Cases
+console.log(argumentsLength(5)); // 1
+console.log(argumentsLength({}, null, "3")); // 3
+console.log(argumentsLength()); // 0
